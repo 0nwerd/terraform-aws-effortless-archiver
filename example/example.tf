baee47f4b887eeb8f3e24195a9de486514b5d14e
Louis Fiori <56321138+0nwerd@users.noreply.github.com>
2023-11-28 12:49:03 +0100

    Update example.tf

--- example/example.tf
+++ example/example.tf
@@ -7,6 +7,6 @@ module "example" {
   region     = "us-east-1"
   account_id = data.aws_caller_identity.current.account_id
 
-  s3_bucket_arn = "YOUR_BUCKET_ARN"
-  ledger_name   = "YOUR_LEDGER_NAME"
+  s3_bucket_name = "YOUR_BUCKET_ARN"
+  ledger_name    = "YOUR_LEDGER_NAME"
 }
